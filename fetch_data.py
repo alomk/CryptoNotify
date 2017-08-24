e9c8b3c43ab32b85063363c8a854738aabc89c4c
Kazi <alomk@bxscience.edu>
2017-08-24 20:45:27 +0000

    finished half of volume grapher

--- fetch_data.py
+++ fetch_data.py
@@ -4,17 +4,50 @@ import json
 import sys
 import time
 import requests
+import datetime
+import pickle
 
 def update():
+    global interval
+    global volumes
+    global times
 
+    data = json.loads(requests.get('https://bittrex.com/api/v1.1/public/getmarketsummaries').text)
+    timestamp = str(datetime.datetime.now())
 
-def drawGraphs():
+    del volumes
+    volumes = {}
+    del times
+    times = []
 
+    for i in data['result']:
+        volumes[i['MarketName']] = [i['BaseVolume']]
 
-def calcDeriv():
+    for i in range(48):
+        time.sleep(interval)
+        data = json.loads(requests.get('https://bittrex.com/api/v1.1/public/getmarketsummaries').text)
+        for j in data['result']:
+            volumes[j['MarketName']].append(j['BaseVolume'])
+            times.append(int(time.time()))
+            open('/home/kazi/CryptoNotify/data/%s'%("test"), 'w').close()
+            open('/home/kazi/CryptoNotify/data/%s'%("test"), 'w').close()
+            outputVol = open('/home/kazi/CryptoNotify/data/%s'%(timestamp),'w')
+            outputTime = open('/home/kazi/CryptoNotify/data/%s_times'%(timestamp),'w')
+            pickle.dump(volumes,outputVol)
+            pickle.dump(times,outputTime)
+            #outputVol.write(volumes)
+            #outputTime.write(times)
+            outputVol.close()
+            outputTime.close()
+        print "finished with iteration %i"%(i)
+        
+    
 
+    #update()
 
-def saveGraphs():
+volumes = {}
+times = []
 
+interval = 1800 #in seconds
 
-interval = 60 #in seconds
+update()
