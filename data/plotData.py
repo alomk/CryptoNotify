5a1369a2f9d9d006199c51476ab852913345b665
Kazi <alomk@bxscience.edu>
2017-08-25 16:23:21 +0000

    added primitive data plotter

--- data/plotData.py
+++ data/plotData.py
@@ -1,8 +1,14 @@
 import sys
 import pickle
+import time
 
 volumes = pickle.load(open(sys.argv[1],'r'))
 times = pickle.load(open(sys.argv[1] + "_times",'r'))
 
+coin = str(sys.argv[2].upper())
 print volumes
 print times
+print len(times)
+
+for i in volumes[coin]:
+    print i
